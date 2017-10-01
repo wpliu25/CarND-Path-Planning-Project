33ef7209608fd93333749a5a83bd784762b1e4fd
mvp <wen.p.liu@gmail.com>
2017-09-30 20:15:36 -0700

    updated car class

---
+++ src/car.h
@@ -0,0 +1,59 @@
+#ifndef CAR_H
+#define CAR_H
+
+#include <vector>
+
+using namespace std;
+
+class Car {
+
+protected:
+
+    int id_;
+    double x_;
+    double y_;
+    double s_;
+    double d_;
+    double yaw_;
+    double vx_;
+    double vy_;
+
+    vector<double> previous_s_;
+    vector<double> previous_d_;
+
+public:
+    Car(){};
+    Car(int id, double x, double y, double s, double d, double yaw, double vx = 0, double vy = 0)
+    {
+        id_ = id;
+        x_ = x;
+        y_ = y;
+        s_ = s;
+        d_ = d;
+        yaw_ = yaw;
+        if(vx_ != vx) vx_ = vx;
+        if(vy_ != vy) vx_ = vy;
+    };
+    ~Car(){};
+
+    void UpdateCar(double x, double y, double s, double d, double yaw, double vx = 0, double vy = 0)
+    {
+        x_ = x;
+        y_ = y;
+        s_ = s;
+        d_ = d;
+        yaw_ = yaw;
+        if(vx_ != vx) vx_ = vx;
+        if(vy_ != vy) vx_ = vy;
+    };
+
+    void UpdateS(double s){s_ = s;};
+
+    double GetX(){return x_;};
+    double GetY(){return y_;};
+    double GetS(){return s_;};
+    double GetYaw(){return yaw_;};
+
+};
+
+#endif // CAR_H
