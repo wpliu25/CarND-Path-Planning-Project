d47a8531cd24f88d73a29d6254dad68e81d41571
mvp <wen.p.liu@gmail.com>
2017-09-30 20:16:40 -0700

    updated main.cpp to use utils, car and path

--- src/main.cpp
+++ src/main.cpp
@@ -1,165 +1,7 @@
-#include <fstream>
-#include <math.h>
-#include <uWS/uWS.h>
-#include <chrono>
-#include <iostream>
-#include <thread>
-#include <vector>
-#include "Eigen-3.3/Eigen/Core"
-#include "Eigen-3.3/Eigen/QR"
-#include "json.hpp"
+#include "utils.h"
 #include "spline.h"
 #include "car.h"
-
-using namespace std;
-
-// for convenience
-using json = nlohmann::json;
-
-// For converting back and forth between radians and degrees.
-constexpr double pi() { return M_PI; }
-double deg2rad(double x) { return x * pi() / 180; }
-double rad2deg(double x) { return x * 180 / pi(); }
-
-// Checks if the SocketIO event has JSON data.
-// If there is data the JSON object in string format will be returned,
-// else the empty string "" will be returned.
-string hasData(string s) {
-    auto found_null = s.find("null");
-    auto b1 = s.find_first_of("[");
-    auto b2 = s.find_first_of("}");
-    if (found_null != string::npos) {
-        return "";
-    } else if (b1 != string::npos && b2 != string::npos) {
-        return s.substr(b1, b2 - b1 + 2);
-    }
-    return "";
-}
-
-double distance(double x1, double y1, double x2, double y2)
-{
-    return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-}
-int ClosestWaypoint(double x, double y, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-
-    double closestLen = 100000; //large number
-    int closestWaypoint = 0;
-
-    for(int i = 0; i < maps_x.size(); i++)
-    {
-        double map_x = maps_x[i];
-        double map_y = maps_y[i];
-        double dist = distance(x,y,map_x,map_y);
-        if(dist < closestLen)
-        {
-            closestLen = dist;
-            closestWaypoint = i;
-        }
-
-    }
-
-    return closestWaypoint;
-
-}
-
-int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-
-    int closestWaypoint = ClosestWaypoint(x,y,maps_x,maps_y);
-
-    double map_x = maps_x[closestWaypoint];
-    double map_y = maps_y[closestWaypoint];
-
-    double heading = atan2( (map_y-y),(map_x-x) );
-
-    double angle = abs(theta-heading);
-
-    if(angle > pi()/4)
-    {
-        closestWaypoint++;
-    }
-
-    return closestWaypoint;
-
-}
-
-// Transform from Cartesian x,y coordinates to Frenet s,d coordinates
-vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-    int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
-
-    int prev_wp;
-    prev_wp = next_wp-1;
-    if(next_wp == 0)
-    {
-        prev_wp  = maps_x.size()-1;
-    }
-
-    double n_x = maps_x[next_wp]-maps_x[prev_wp];
-    double n_y = maps_y[next_wp]-maps_y[prev_wp];
-    double x_x = x - maps_x[prev_wp];
-    double x_y = y - maps_y[prev_wp];
-
-    // find the projection of x onto n
-    double proj_norm = (x_x*n_x+x_y*n_y)/(n_x*n_x+n_y*n_y);
-    double proj_x = proj_norm*n_x;
-    double proj_y = proj_norm*n_y;
-
-    double frenet_d = distance(x_x,x_y,proj_x,proj_y);
-
-    //see if d value is positive or negative by comparing it to a center point
-
-    double center_x = 1000-maps_x[prev_wp];
-    double center_y = 2000-maps_y[prev_wp];
-    double centerToPos = distance(center_x,center_y,x_x,x_y);
-    double centerToRef = distance(center_x,center_y,proj_x,proj_y);
-
-    if(centerToPos <= centerToRef)
-    {
-        frenet_d *= -1;
-    }
-
-    // calculate s value
-    double frenet_s = 0;
-    for(int i = 0; i < prev_wp; i++)
-    {
-        frenet_s += distance(maps_x[i],maps_y[i],maps_x[i+1],maps_y[i+1]);
-    }
-
-    frenet_s += distance(0,0,proj_x,proj_y);
-
-    return {frenet_s,frenet_d};
-
-}
-
-// Transform from Frenet s,d coordinates to Cartesian x,y
-vector<double> getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-    int prev_wp = -1;
-
-    while(s > maps_s[prev_wp+1] && (prev_wp < (int)(maps_s.size()-1) ))
-    {
-        prev_wp++;
-    }
-
-    int wp2 = (prev_wp+1)%maps_x.size();
-
-    double heading = atan2((maps_y[wp2]-maps_y[prev_wp]),(maps_x[wp2]-maps_x[prev_wp]));
-    // the x,y,s along the segment
-    double seg_s = (s-maps_s[prev_wp]);
-
-    double seg_x = maps_x[prev_wp]+seg_s*cos(heading);
-    double seg_y = maps_y[prev_wp]+seg_s*sin(heading);
-
-    double perp_heading = heading-pi()/2;
-
-    double x = seg_x + d*cos(perp_heading);
-    double y = seg_y + d*sin(perp_heading);
-
-    return {x,y};
-
-}
+#include "path.h"
 
 int main() {
     uWS::Hub h;
@@ -217,8 +59,9 @@ int main() {
     change_right.push_back(1);
     change_right.push_back(0);
     Car car;
+    Path path;
 
-    h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy, &lane, &ref_vel, &change_lanes, &change_left, &change_right, &car](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+    h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy, &lane, &ref_vel, &change_lanes, &change_left, &change_right, &car, &path](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                 uWS::OpCode opCode) {
         // "42" at the start of the message means there's a websocket message event.
         // The 4 signifies a websocket message
@@ -244,7 +87,10 @@ int main() {
                     double car_d = j[1]["d"];
                     double car_yaw = j[1]["yaw"];
                     //double car_speed = j[1]["speed"];
-                    car.updateCar(car_x, car_y, car_s, car_d, car_yaw);
+                    //----------
+                    // Car
+                    //----------
+                    car.UpdateCar(car_x, car_y, car_s, car_d, car_yaw);
 
                     // Previous path data given to the Planner
                     auto previous_path_x = j[1]["previous_path_x"];
@@ -269,7 +115,7 @@ int main() {
                     //----------------------------------------------------------------------------------------
                     if(prev_size > 0)
                     {
-                        car.updateS(end_path_s);
+                        car.UpdateS(end_path_s);
                     }
                     bool too_close = false;
                     bool change_left_safe = true;
@@ -292,7 +138,7 @@ int main() {
                         {
 
                             // check s values greater than our car and s gap of 30m
-                            if((check_car_s_next > car.getS()) && (check_car_s_next-car.getS() < 30))
+                            if((check_car_s_next > car.GetS()) && (check_car_s_next-car.GetS() < 30))
                             {
                                 // flag to handle collisions and cold starts
                                 // also flag to try to change lanes
@@ -306,8 +152,8 @@ int main() {
                         if((lane-1 >= 0) && d < (2+4*(lane-1)+2) && d > (2+4*(lane-1) -2))
                         {
                             // check s values greater than our car and s gap of 30m
-                            if(!(((check_car_s_next > car.getS()) && (check_car_s_next-car.getS() > 30)) ||
-                                 ((check_car_s_next < car.getS()) && (car.getS() - check_car_s_next > 30))))
+                            if(!(((check_car_s_next > car.GetS()) && (check_car_s_next-car.GetS() > 30)) ||
+                                 ((check_car_s_next < car.GetS()) && (car.GetS() - check_car_s_next > 30))))
                             {
                                 change_left_safe = false;
                             }
@@ -316,8 +162,8 @@ int main() {
                         else if((lane + 1 <= 2) && d < (2+4*(lane+1)+2) && d > (2+4*(lane+1) -2))
                         {
                             // check s values greater than our car and s gap of 30m
-                            if(!(((check_car_s_next > car.getS()) && (check_car_s_next-car.getS() > 30)) ||
-                                 ((check_car_s_next < car.getS()) && (car.getS() - check_car_s_next > 30))))
+                            if(!(((check_car_s_next > car.GetS()) && (check_car_s_next-car.GetS() > 30)) ||
+                                 ((check_car_s_next < car.GetS()) && (car.GetS() - check_car_s_next > 30))))
                             {
                                 change_right_safe = false;
                             }
@@ -350,54 +196,26 @@ int main() {
                         ref_vel += 0.224;
                     }
 
-                    //----------------------------------------------------------------------------------------
+                    //================================================================================================
+                    //----------
+                    // Path
+                    //----------
 
                     // list of a widely spaced (x,y) waypoints, evenly spaced at 30m to be interpolated with a spline
                     vector<double> ptsx;
                     vector<double> ptsy;
 
                     // reference x, y, yaw states
-                    double ref_x = car.getX();
-                    double ref_y = car.getY();
-                    double ref_yaw = deg2rad(car.getYaw());
+                    double ref_x = car.GetX();
+                    double ref_y = car.GetY();
+                    double ref_yaw = deg2rad(car.GetYaw());
 
-                    // if previous size is almost empty, use the car as starting reference
-                    if(prev_size < 2)
-                    {
-                        // use 2 points that make the path tanget to the car
-                        double prev_car_x = car.getX() - cos(car_yaw);
-                        double prev_car_y = car.getY() - sin(car_yaw);
-
-                        ptsx.push_back(prev_car_x);
-                        ptsx.push_back(car.getX());
-
-                        ptsy.push_back(prev_car_y);
-                        ptsy.push_back(car.getY());
-                    }
-                    // else use the previous path's end point as starting reference
-                    else
-                    {
-                        // redefine reference state as previous path end point
-                        ref_x = previous_path_x[prev_size-1];
-                        ref_y = previous_path_y[prev_size-1];
-
-                        double ref_x_prev = previous_path_x[prev_size-2];
-                        double ref_y_prev = previous_path_y[prev_size-2];
-                        ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
-
-                        // use 2 points that make the path tangent to the previous path's end point
-
-                        ptsx.push_back(ref_x_prev);
-                        ptsx.push_back(ref_x);
-
-                        ptsy.push_back(ref_y_prev);
-                        ptsy.push_back(ref_y);
-                    }
+                    path.CalculatePath(car, ptsx, ptsy, ref_x, ref_y, ref_yaw, prev_size, previous_path_x, previous_path_y);
 
                     // in Frenet add evenly 30m spaced points ahead of the starting reference
-                    vector<double> next_wp0 = getXY(car.getS()+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
-                    vector<double> next_wp1 = getXY(car.getS()+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
-                    vector<double> next_wp2 = getXY(car.getS()+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                    vector<double> next_wp0 = getXY(car.GetS()+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                    vector<double> next_wp1 = getXY(car.GetS()+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                    vector<double> next_wp2 = getXY(car.GetS()+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
 
                     ptsx.push_back(next_wp0[0]);
                     ptsx.push_back(next_wp1[0]);
